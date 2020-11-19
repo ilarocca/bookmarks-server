674f756790a552bc0f9f52d4735ba3c2a3bff42d
Ivan La Rocca <ivanlarocca12@gmail.com>
2020-11-18 20:05:23 -0500

    refactor for migrations, tests, knex integration for GET and G GET and GET by id

---
+++ seeds/seed.bookmarks.sql
@@ -0,0 +1,13 @@
+
+insert into bookmarks (url, title, rating, description) 
+values 
+  ('www.youtube.com', 'Youtube', '5', 'site for videos'),
+  ('www.reddit.com', 'Reddit', '5', 'site for community'),
+  ('www.google.com', 'Google', '5', 'site for searching'), 
+  ('www.thinkful.com', 'Thinkful', '5', 'site for learning'), 
+  ('www.github.com', 'Github', '5', 'site for projects'), 
+  ('www.uuidgenerator.net', 'UUID', '5', 'site for ids'),
+  ('www.netflix.com', 'Netflix', '5', 'site for watching'), 
+  ('www.hulu.com', 'Hulu', '4', 'site for watching, but not as good'), 
+  ('www.amazon.com', 'Amazon', '5', 'site for shopping'), 
+  ('www.stackoverflow.com', 'StackOverflow', '5', 'site for keeping me sane');
